cc3bfac03a862a3406af0abf9c12c9b6adb8f37a
Rachel P.Y. Liao <u3512485@connect.hku.hk>
2016-11-20 04:23:54 -0500

    basic control panel

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -6,16 +6,29 @@ import React, { Component } from 'react'
 
 import { Center, Col, Row } from 'components/Flex'
 import Grid from 'components/Grid'
-
+import ControlPanel from 'components/ControlPanel'
 
 export default class App extends Component {
   constructor () {
     super()
+
+    this.state = {
+      fullWidth: document.documentElement.clientWidth,
+      chartProperties: null,
+    }
+  }
+
+  plot(params) {
+    console.log('plot', params)
+    this.setState({chartProperties: params})
   }
 
   render () {
     return (
-      <Grid />
+      <div style={{width: this.state.fullWidth}}>
+        <ControlPanel plot={(params) => this.plot(params)}/>
+        <Grid />
+      </div>
     )
   }
 }
---
+++ ui/src/components/ControlPanel.js
@@ -0,0 +1,118 @@
+// Dependencies
+
+import React, { Component } from 'react'
+import * as ReactBootstrap from 'react-bootstrap'
+import { Navbar, NavDropdown, Nav, MenuItem, Tab, NavItem } from 'react-bootstrap'
+
+// Components
+
+import { Center, Col, Row } from 'components/Flex'
+
+class NavBar extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      areas: [],
+      area: '',
+      serverObjs: [],
+      servers: [],
+      server: null,
+      dataTypes: ['performance', 'impressions'],
+      dataType: 'performance',
+    }
+  }
+
+  componentWillMount() {
+    console.log("componentWillMount ...")
+    this.fetchServers()
+  }
+
+  fetchServers() {
+    const url = 'http://localhost:8000/servers'
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+      const serverObjs = json.data
+
+      let servers = []
+      let areas = []
+      serverObjs.forEach((server) => {
+        servers.push(server.id)
+        areas[server.dc] = true
+      })
+
+      this.setState({ areas: Object.keys(areas), area: serverObjs[0].dc, serverObjs, servers, server: servers[0]})
+      this.props.plot({server: servers[0], area: serverObjs[0].dc, dataType: this.state.dataType})
+    })
+    .catch(err => { console.log('ERROR', err); });
+  }
+
+  getDropdown(eventKey, title, id) {
+    const list = this.state[`${eventKey}s`]
+
+    let menuItems = []
+    list.forEach((item,i) => {
+      menuItems.push(<MenuItem eventKey={`${eventKey}.${i}`}>{item}</MenuItem>)
+    })
+    return (
+      <NavDropdown
+        eventKey={eventKey}
+        title={title}
+        id={id}
+        onSelect={(eventKey) => {
+          console.log(eventKey)
+          const key = eventKey.slice(0,eventKey.indexOf('.'))
+          const index = eventKey.slice(eventKey.indexOf('.')+1)
+
+          let stateObj = this.state[`${key}s`]
+          stateObj[key] = this.state[`${key}s`][index]
+          this.setState(stateObj)
+        }}
+      >
+        {menuItems}
+      </NavDropdown>
+    )
+  }
+
+  render() {
+    const { server, area, dataType } = this.state
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Board</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          {this.getDropdown('area', `Area ${area}`, "area-dropdown")}
+          {this.getDropdown('server', `Server ${server}`, "server-dropdown")}
+          {this.getDropdown('dataType', `Data Type ${dataType}`, "data-type-dropdown")}
+          <NavItem
+            onClick={() => {
+              this.props.plot({server, area, dataType})
+            }}
+          >
+            <p style={{margin: 0, color: '#FFC107', fontSize: '24'}}>Plot!</p>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    )
+  }
+}
+
+export default class ControlPanel extends Component {
+  constructor () {
+    super()
+  }
+
+  componentWillMount() {
+    console.log("componentWillMount ...")
+  }
+
+  render () {
+    return (
+      <NavBar plot={this.props.plot}/>
+    )
+  }
+}
